000a5dd5475f0ddd293b300aa5cd92ba30958f79
rupesh <rupesh.ruppi@gmail.com>
2020-02-28 09:24:07 +0530

    change

--- src/main/java/com/assigment/payload/LoginRequest.java
+++ src/main/java/com/assigment/payload/LoginRequest.java
@@ -4,7 +4,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 /**
- * Created by rajeevkumarsingh on 02/08/17.
  */
 public class LoginRequest {
     @NotBlank
